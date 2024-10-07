c8107f0f38c797e1a747b36ce3752efabc8b5c41
ntap <topkid1212@gmail.com>
2024-10-07 15:17:02 +0900

    weapon.cs 파일들

---
+++ Assets/Test_Team15/Script/Weapon/MainState/IWeapon.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IWeapon
+{
+    void Update();
+    void ChangeState(WeaponState newState);
+    void Attack();
+}
+
